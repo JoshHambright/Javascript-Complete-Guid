d3e1937cc5e7da73dd699ba3be91553898e9c1e4
JoshHambright <josh.hambright@gmail.com>
2021-01-21 14:33:33 -0500

    calculator basic functionality

--- Section 2/basics-01-starting-project/assets/scripts/app.js
+++ Section 2/basics-01-starting-project/assets/scripts/app.js
@@ -1,11 +1,48 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(){
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+//Get the input from the field
+function getUserInput(){
+    return +userInput.value;
 }
 
+//Build the output string
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription);
+}
+//Addition
+function add(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber)
+}
+//Subtraction
+function subtract(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber)
+}
+//Multiplication
+function multiply(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber)
+}
+//Division
+function divide(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber)
+}
 
+//Event Listeners
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
