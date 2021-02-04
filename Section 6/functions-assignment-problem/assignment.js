0340a8e3979017c6b2f2f6ef02d93f12dfdee4f2
JoshHambright <josh.hambright@gmail.com>
2021-02-04 13:13:38 -0500

    End of Section 6

--- Section 6/functions-assignment-problem/assignment.js
+++ Section 6/functions-assignment-problem/assignment.js
@@ -1,32 +1,53 @@
 const sayHello = (name = 'HOOMAN') => {
   console.log('Hi ' + name);
-}
-const sayHello2 = (greeting,name) => {
+};
+const sayHello2 = (greeting, name) => {
   console.log(`${greeting} ${name}`);
-}
+};
 const sayHello3 = () => {
   console.log('Hi user!');
-}
-const returnGreeting = (name) =>{
+};
+const returnGreeting = (name) => {
   return `Hi ${name}`;
-}
+};
 const multipleHello = (...strings) => {
-  
   const checkString = (string) => {
-    if (string === ''){ 
-      return 'User'
+    if (string === '') {
+      return 'User';
     }
     return string;
-  }
-  for (string of strings){
-    
+  };
+  for (string of strings) {
     console.log('Hi ' + checkString(string));
+  }
+};
+
+function checkInput(cb, ...string) {
+  let hasEmptyText = false;
+  for (const text of string) {
+    if (!text) {
+      hasEmptyText = true;
+      break;
+    }
+  }
+  if (!hasEmptyText) {
+    cb();
   }
 }
 
 sayHello('Josh');
-sayHello2('Hello','Max');
+sayHello2('Hello', 'Max');
 sayHello3();
 console.log(returnGreeting('Josh'));
 sayHello();
-multipleHello('bloop','blop','','josh');
+multipleHello('bloop', 'blop', '', 'josh');
+
+checkInput(
+  () => {
+    console.log('All not empty');
+  },
+  'hello',
+  '12',
+  'adsa',
+  ''
+);
